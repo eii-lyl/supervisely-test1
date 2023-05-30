2e8ab9e8ce63acc5605d3e8c91b3e22ad15f1db1
lyl <lyu.yongliang201856eii@gmail.com>
2023-05-30 16:14:22 +0900

    first commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from art import tprint
+import supervisely as sly
+
+# load ENV variables for debug
+# has no effect in production
+if sly.is_development():
+    load_dotenv("local.env")
+
+
+def main():
+    name = sly.env.user_login()
+    print("Hello World! This app is run by the user:")
+    tprint(name)
+
+
+if __name__ == "__main__":
+    main()
